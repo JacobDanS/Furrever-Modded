35e49dcdbc43126e78383640450a6e35d236d1d6
ItzKORE <jacobdschenke@gmail.com>
2017-06-25 13:01:20 -0500

    3465346
    34636

--- src/main/java/korejjamjar/furrevermodded/FurreverModded.java
+++ src/main/java/korejjamjar/furrevermodded/FurreverModded.java
@@ -5,6 +5,7 @@ import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.item.Item;
 
 @Mod(modid = "kfmm", name = "Furrever Modded", version = "1.0")
@@ -16,7 +17,8 @@ public class FurreverModded {
 	public void preInit(FMLPreInitializationEvent event) {
 		//Item/Block init and registering
 		//Config handling
-		itemFidgetCube = new ItemFidgetCube();
+		itemFidgetCube = new ItemFidgetCube().setUnlocalizedName("ItemFidgetCube");
+		GameRegistry.registerItem(itemFidgetCube, itemFidgetCube.getUnlocalizedName().substring(5));
 	}
 	
 	@EventHandler
